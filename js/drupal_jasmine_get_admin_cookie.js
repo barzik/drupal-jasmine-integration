53760700cab0796dbe16fd758bfc44add43d64fd
Ran Bar-Zik <ran@bar-zik.com>
2013-10-07 15:34:15 +0200

    Update drupal_jasmine_get_admin_cookie.js
    Added support for registered user and drupal url. phantomJS support is completed.

--- js/drupal_jasmine_get_admin_cookie.js
+++ js/drupal_jasmine_get_admin_cookie.js
@@ -33,45 +33,89 @@ function waitFor(testFx, onReady, timeOutMillis) {
         }, 100); //< repeat check every 100ms
 };
 
-if (system.args.length !== 3) {
-    console.log('Usage: hpln_jasmine_get_admin_cookie.js URL + key');
+if (system.args.length !== 4) {
+    console.log('Usage: hpln_jasmine_get_admin_cookie.js drupal_url + User + PWD');
     phantom.exit(1);
 }
 
 var page = require('webpage').create();
 
-// Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
+
+var drupal_url = system.args[1];
+var drupal_username=  system.args[2];
+var drupal_password=  system.args[3];
+var page = new WebPage(), testindex = 0, loadInProgress = false;
+
 page.onConsoleMessage = function(msg) {
-    console.log(msg);
+  console.log(msg);
 };
 
-var url = system.args[1]+'&'+system.args[2] ;
+page.onLoadStarted = function() {
+  loadInProgress = true;
+  console.log("load started");
+};
 
-page.open(url, function (status) {
-    //Page is loaded!
-    console.log('Loading a web page');
-    if (status !== "success") {
-        console.log("Unable to access network");
-        phantom.exit();
-    } else {
-        waitFor(function(){
-            return page.evaluate(function(){
-                return document.body.querySelector('.symbolSummary .pending') === null
-            });
-        }, function(){
-            var exitCode = page.evaluate(function(){
-                var header = document.body.querySelectorAll('#user-sso-login-name');
-                if (header && header.length > 0) {
+page.onLoadFinished = function() {
+  loadInProgress = false;
+  console.log("load finished");
+};
+
+/*
+ * This function wraps WebPage.evaluate, and offers the possibility to pass
+ * parameters into the webpage function. The PhantomJS issue is here:
+ * 
+ *   http://code.google.com/p/phantomjs/issues/detail?id=132
+ * 
+ * This is from comment #43.
+ */
+function evaluate(page, func) {
+    var args = [].slice.call(arguments, 2);
+    var fn = "function() { return (" + func.toString() + ").apply(this, " + JSON.stringify(args) + ");}";
+    return page.evaluate(fn);
+}
+
+var steps = [
+  function() {
+    //Load Login Page
+    page.open(drupal_url);
+  },
+  function() {
+    //Login
+    evaluate(page, function(drupal_username, drupal_password) {
+		var form = document.getElementById('user-login');
+		console.log("password : "+drupal_password);
+		console.log("name : "+drupal_username);
+		document.getElementById('edit-name').value=drupal_username;
+		document.getElementById('edit-pass').value =drupal_password;
+		form.submit(); 
+    }, drupal_username, drupal_password);
+  }, 
+  function() {
+    // If logged in - return 0. if not an error
+    page.evaluate(function() {
+   // console.log(document.querySelectorAll('html')[0].outerHTML);
+                if (document.getElementsByClassName('logged-in').length == 1){
                     console.log('Session captured');
                     return 0;
                 } else {
                     console.log('Session was not captured');
                     return 1;
                 }
+	  
+	  
+    });
+  }
+];
 
-            });
-            phantom.exit(exitCode);
-        });
 
-    }
-});
+interval = setInterval(function() {
+  if (!loadInProgress && typeof steps[testindex] == "function") {
+    console.log("step " + (testindex + 1));
+    steps[testindex]();
+    testindex++;
+  }
+  if (typeof steps[testindex] != "function") {
+    console.log("test complete!");
+    phantom.exit();
+  }
+}, 50);
